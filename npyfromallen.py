58c91c284885ca46afcb87f4477771b5a106202b
Joel Walsh <joelawalsh@gmail.com>
2021-04-09 15:13:46 -0700

    Add files via upload

---
+++ npyfromallen.py
@@ -0,0 +1,31 @@
+import allennlp
+
+#import networkx as nx
+from allennlp.predictors.predictor import Predictor
+import allennlp_models.tagging
+import csv
+import numpy as np
+
+
+if __name__ == "__main__":
+	
+	text = open("firstgrade.txt").read()
+	      
+	
+	predictor = Predictor.from_path("https://storage.googleapis.com/allennlp-public-models/coref-spanbert-large-2021.03.10.tar.gz")
+	prediction = predictor.predict(document=text)
+
+	print(predictor.top_spans)
+	w = csv.writer(open("output.csv", "w"))	
+	for key, val in prediction.items():
+		w.writerow([key, val])
+
+	f = open("dict.txt","w")
+	f.write( str(prediction) )
+
+	f.close()
+
+	np.save('first_grade.npy', prediction) 
+
+   
+
